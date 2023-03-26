a7b36e6f008f61ee4f8c3c94da539438ffe247dd
serbanlex <serbanlex2001@yahoo.com>
2023-03-26 18:13:44 +0300

    Add backend Spring server initializing

---
+++ backend/src/main/java/ic/losingenieros/fixitbackend/FixItBackendApplication.java
@@ -0,0 +1,13 @@
+package ic.losingenieros.fixitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixItBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixItBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/ic/losingenieros/fixitbackend/FixItBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ic.losingenieros.fixitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixItBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
