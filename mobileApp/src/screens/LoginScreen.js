2d647ac49f21b3edaee4b4bd605a95ee432ddd81
m-rusu-st <vasilica.rusu@student.upt.ro>
2023-04-24 17:55:23 +0300

    Added user data inputs and buttons to login screen

--- mobileApp/src/screens/LoginScreen.js
+++ mobileApp/src/screens/LoginScreen.js
@@ -1,29 +1,68 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import React, { useState, Component } from 'react';
+import { StyleSheet, Text, View, ImageBackground, ScrollView } from 'react-native';
 import { Button } from 'native-base';
+import CustomInput from '../components/CustomInput';
+import CustomButton from '../components/CustomButton';
 
 function LoginScreen({ navigation }) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onLogInPressed = () => {
+        console.warn('Log In Pressed');
+    }
+    const onForgotPasswordPressed = () => {
+        console.warn('Forgot Password Pressed');
+    }
+    const onRegisterPressed = () => {
+        console.warn('Register Pressed');
+    }
+
     return (
+    <ScrollView showsVerticalScrollIndicator={false} >
         <View style={styles.container}>
-            <Text>Login Screen</Text>
+            <Text style={styles.title}>Log In</Text>
             <Button
                 style={{ backgroundColor: '#00fff', position: 'absolute', top: 40, left: 20 }}
                 onPress={() => navigation.goBack()}>
                 <Text style={styles.buttonText}>Back</Text>
             </Button>
+            <CustomInput
+            placeholder="Username"
+            value={username}
+            setValue={setUsername}
+            />
+            <CustomInput
+            placeholder="Password"
+            value={password}
+            setValue={setPassword}
+            secureTextEntry={true}
+            />
+
+            <CustomButton text="Log In" onPress={onLogInPressed} />
+            <CustomButton text="Forgot Password?" onPress={onForgotPasswordPressed} type="TERTIARY" />
+            <CustomButton text="Don't have an account? Create One" onPress={onRegisterPressed} type="TERTIARY" />
         </View>
+    </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
     flex: 1,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingTop: 100,
+    },
+    title: {
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 36,
+    marginBottom: 50,
     },
     buttonText: {
-    color: '#fff',
+    color: '#43428b',
     fontWeight: 'bold',
     fontSize: 20,
     },
--- mobileApp/src/screens/WelcomeScreen.js
+++ mobileApp/src/screens/WelcomeScreen.js
@@ -8,19 +8,14 @@ import LoginScreen from './LoginScreen.js';
 function HomeScreen({ navigation }) {
     return (
         <View style={styles.container}>
-            <ImageBackground
-                source={{ uri: 'https://images.unsplash.com/photo-1622129710676-16a6b2014aec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' }}
-                style={styles.backgroundImage}
-            >
-                <View style={styles.overlay} />
-                <Text style={styles.title}>Welcome to Fix-It!</Text>
-                <Button style={styles.button} onPress={() => navigation.navigate('Login')}>
-                    <Text style={styles.buttonText}>Login</Text>
-                </Button>
-                <Button style={styles.button} onPress={() => navigation.navigate('Register')}>
-                    <Text style={styles.buttonText}>Register</Text>
-                </Button>
-            </ImageBackground>
+            <View style={styles.overlay} />
+            <Text style={styles.title}>Welcome to Fix-It!</Text>
+            <Button style={styles.button} onPress={() => navigation.navigate('Login')}>
+                <Text style={styles.buttonText}>Login</Text>
+            </Button>
+            <Button style={styles.button} onPress={() => navigation.navigate('Register')}>
+                <Text style={styles.buttonText}>Register</Text>
+            </Button>
         </View>
     );
 };
@@ -41,12 +36,7 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-    },
-    backgroundImage: {
-        flex: 1,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#43428b',
     },
     overlay: {
         ...StyleSheet.absoluteFill,
@@ -56,7 +46,7 @@ const styles = StyleSheet.create({
         fontSize: 36,
         color: '#fff',
         fontWeight: 'bold',
-        marginBottom: 50,
+        marginBottom: 200,
         textAlign: 'center',
     },
     button: {
