532902311f95160c4c93d56e85d01fcd8c538b70
m-rusu <rusu_mela2001@yahoo.com>
2023-05-26 13:09:35 +0300

    Modified register login depending on the role chosen

---
+++ mobileApp/src/screens/RegisterOffererScreen.js
@@ -0,0 +1,97 @@
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, ImageBackground, ScrollView, Alert } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import CustomInput from '../components/CustomInput';
+import CustomButton from '../components/CustomButton';
+import { useNavigation } from '@react-navigation/native';
+
+
+const RegisterOffererScreen = ({ route }) => {
+  const { control, handleSubmit, watch } = useForm();
+  const genericUserData = route.params.data;
+  const navigation = useNavigation();
+
+  const onRegisterPressed = async data => {
+    data = { ...genericUserData, ...data };
+    delete data['password-repeat'];
+    console.log(data)
+    try {
+        const response = await fetch(`${API_URL}/serviceOfferers`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+          });
+          
+          if (response.ok) {
+            navigation.navigate('SuccessfulLogin');
+          } else {
+            const errorResponse = await response.json();
+            console.log(errorResponse);
+            Alert.alert('Registration Error', `Failed to register service offerer. Reason: ${errorResponse.error}`);
+          }
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Registration Error', 'Failed to register service offerer.');
+    }
+  };
+
+
+  return (
+    <View style={styles.container}>
+      <CustomInput
+        name="firmName"
+        control={control}
+        placeholder="Firm Name"
+        rules={{
+          required: 'Firm name is required',
+        }}
+      />
+      <CustomInput
+        name="firmCity"
+        control={control}
+        placeholder="Firm City"
+        rules={{
+          required: 'Firm city is required',
+        }}
+      />
+      <CustomInput
+        name="firmAddress"
+        control={control}
+        placeholder="Firm Address"
+        rules={{
+          required: 'Firm address is required',
+        }}
+      />
+      <CustomInput
+        name="CUI"
+        control={control}
+        placeholder="CUI"
+        rules={{
+          required: 'CUI is required',
+        }}
+      />
+      <CustomInput
+        name="CAEN"
+        control={control}
+        placeholder="CAEN"
+        rules={{
+          required: 'CAEN is required',
+        }}
+      />
+
+      <CustomButton text="Register" onPress={handleSubmit(onRegisterPressed)} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default RegisterOffererScreen;
--- mobileApp/src/screens/RegisterScreen.js
+++ mobileApp/src/screens/RegisterScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, Component } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, ImageBackground, ScrollView, Alert } from 'react-native';
 import { Button } from 'native-base';
 import CustomInput from '../components/CustomInput';
@@ -10,108 +10,127 @@ import GradientBackground from '../components/GradientBackground2';
 const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 API_URL="http://192.168.100.71:3000";
 
-function RegisterScreen({ }) {
+const RegisterScreen = ({ route }) => {
   const navigation = useNavigation();
-
+  const role = route.params.role;
+  const [genericUserData, setGenericUserData] = useState('');
   const { control, handleSubmit, watch } = useForm();
   const pwd = watch('password');
 
-  const onRegisterPressed = async data => {
-    console.log(data);
-    try {
-      console.log("****" + API_URL);
-      const response = await fetch(`${API_URL}/clients`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      }).then(response => {
+  const onNextPressed = (data) => {
+    setGenericUserData(data);
+    console.log("########", genericUserData);
+    console.log("########", data);
+    navigation.navigate('RegisterOfferer', { data: data });
+  };
 
-        if (response.ok) {
-          navigation.navigate('SuccessfulLogin');
-        } else {
-          Alert.alert('Registration Error', 'Failed to register user.');
-        }
-      });
+  const onRegisterPressed = async data => {
+    try {
+        const response = await fetch(`${API_URL}/clients`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+          });
+          
+          if (response.ok) {
+            navigation.navigate('SuccessfulLogin');
+          } else {
+            const errorResponse = await response.json();
+            console.log(errorResponse);
+            Alert.alert('Registration Error', `Failed to register client. Reason: ${errorResponse.error}`);
+          }
     } catch (error) {
       console.error(error);
-      Alert.alert('Registration Error', 'Failed to register user.');
+      Alert.alert('Registration Error', 'Failed to register client.');
     }
   };
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.contentContainer} >
+    <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.contentContainer}>
       <View style={styles.container}>
-      <GradientBackground>
-        <Text style={styles.title}>Register</Text>
-        <Button
-          style={{ backgroundColor: '#00fff', position: 'absolute', top: 0, left: 20 }}
-          onPress={() => navigation.goBack()}>
-          <Text style={styles.buttonText}>Back</Text>
-        </Button>
-        <CustomInput
-          name="firstName"
-          control={control}
-          placeholder="First Name"
-          rules={{
-            required: 'First Name is required',
-            minLength: { value: 3, message: 'First Name must be at least 3 characters long' },
-            maxLength: { value: 21, message: 'First Name must be at most 21 characters long' }
-          }}
-        />
-        <CustomInput
-          name="lastName"
-          control={control}
-          placeholder="Last Name"
-          rules={{
-            required: 'Last Name is required',
-            minLength: { value: 3, message: 'Last Name must be at least 3 characters long' },
-            maxLength: { value: 21, message: 'Last Name must be at most 21 characters long' }
-          }}
-        />
-        <CustomInput
-          name="email"
-          control={control}
-          placeholder="Email"
-          rules={{
-            required: 'Email is required',
-            pattern: { value: EMAIL_REGEX, message: 'Invalid email address' }
-          }}
-        />
-        <CustomInput
-          name="phoneNumber"
-          control={control}
-          placeholder="Phone Number"
-          rules={{ required: 'Phone Number is required' }}
-        />
-        <CustomInput
-          name="password"
-          control={control}
-          placeholder="Password"
-          secureTextEntry={true}
-          rules={{
-            required: 'Password is required',
-            minLength: { value: 8, message: 'Password must be at least 8 characters long' },
-          }}
-        />
-        <CustomInput
-          name="password-repeat"
-          control={control}
-          placeholder="Repeat Password"
-          secureTextEntry={true}
-          rules={{
-            validate: value =>
-              value == pwd || 'Passwords do not match',
-          }}
-        />
+        <GradientBackground>
+          <Text style={styles.title}>Register</Text>
+          <Button
+            style={{ backgroundColor: '#00fff', position: 'absolute', top: 0, left: 20 }}
+            onPress={() => navigation.goBack()}
+          >
+            <Text style={styles.buttonText}>Back</Text>
+          </Button>
+          <CustomInput
+            name="firstName"
+            control={control}
+            placeholder="First Name"
+            rules={{
+              required: 'First Name is required',
+              minLength: { value: 3, message: 'First Name must be at least 3 characters long' },
+              maxLength: { value: 21, message: 'First Name must be at most 21 characters long' }
+            }}
+          />
+          <CustomInput
+            name="lastName"
+            control={control}
+            placeholder="Last Name"
+            rules={{
+              required: 'Last Name is required',
+              minLength: { value: 3, message: 'Last Name must be at least 3 characters long' },
+              maxLength: { value: 21, message: 'Last Name must be at most 21 characters long' }
+            }}
+          />
+          <CustomInput
+            name="email"
+            control={control}
+            placeholder="Email"
+            rules={{
+              required: 'Email is required',
+              pattern: { value: EMAIL_REGEX, message: 'Invalid email address' }
+            }}
+          />
+          <CustomInput
+            name="phoneNumber"
+            control={control}
+            placeholder="Phone Number"
+            rules={{ required: 'Phone Number is required' }}
+          />
+          <CustomInput
+            name="password"
+            control={control}
+            placeholder="Password"
+            secureTextEntry={true}
+            rules={{
+              required: 'Password is required',
+              minLength: { value: 8, message: 'Password must be at least 8 characters long' },
+            }}
+          />
+          <CustomInput
+            name="password-repeat"
+            control={control}
+            placeholder="Repeat Password"
+            secureTextEntry={true}
+            rules={{
+              validate: value =>
+                value == pwd || 'Passwords do not match',
+            }}
+          />
+          {role === "serviceOfferer" && (
+          <>
+            <CustomButton text="Next" onPress={handleSubmit(onNextPressed)} />
+          </>
+          )}
 
-        <CustomButton text="Register" onPress={handleSubmit(onRegisterPressed)} />
+          {role === "client" && (
+          <>
+            <CustomButton text="Register" onPress={handleSubmit(onRegisterPressed)} />
+          </>
+          )}
+
         </GradientBackground>
       </View>
     </ScrollView>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
   container: {
@@ -122,7 +141,7 @@ const styles = StyleSheet.create({
     paddingTop: 40,
   },
   contentContainer: {
-      flexGrow: 1,
+    flexGrow: 1,
   },
   title: {
     color: '#000',
